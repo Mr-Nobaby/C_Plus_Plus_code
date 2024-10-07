b8d04b518142ff3bdf101b6e9415493e837d4934
MFL <mfl15877501770@gmail.com>
2024-10-07 20:28:13 +0800

    控制流

---
+++ Control Flow/src/Main.cpp
@@ -0,0 +1,19 @@
+﻿// HeadFiles.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "Log.h"
+
+
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		if (i>2)
+			return 0;
+		Log("Hello World");
+		std::cout << i << std::endl;
+	}
+    std::cin.get();
+
+}
