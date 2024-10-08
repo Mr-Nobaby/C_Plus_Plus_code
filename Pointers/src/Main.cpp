4f2543bd16af846a8dd3cf7f764e4b83e008ab2f
MFL <mfl15877501770@gmail.com>
2024-10-08 09:32:54 +0800

    指针

---
+++ Pointers/src/Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#define Log(x) std::cout<< x << std::endl
+
+int main()
+{
+	//void* ptr = NULL;
+	//void* ptr = nullptr;
+	//Log(NULL);
+
+	int var = 8;
+	//double* var_ptr = (double*)&var;
+	//void* var_ptr = &var;:
+	int* var_ptr = &var;
+
+	*var_ptr = 10;
+	Log(var);
+
+	char* buf = new char[8];
+	memset(buf, 3, 8);
+
+	char** buf_ptr = &buf;
+	delete[] buf;
+	std::cin.get();
+}
